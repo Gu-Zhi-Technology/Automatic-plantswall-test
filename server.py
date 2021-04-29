96a18977fcfee889c6c407b86d8e813cec42b8a0
jabee0228 <74387204+jabee0228@users.noreply.github.com>
2021-04-29 23:41:04 +0800

    add test

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ server.bind((HOST, PORT))
 server.listen(10)
 conn, addr = server.accept()
 while True:
-
     serverMessage = input()
     serverMessage = serverMessage+'\n'
     conn.sendall(serverMessage.encode())
+    print("ok")
 
