92b70be5fd7feb387ef24f0845ad85542e009c1d
jabee0228 <74387204+jabee0228@users.noreply.github.com>
2021-04-29 22:07:48 +0800

    application

---
+++ application/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    val host = "127.0.0.1"
+    val port = 2338
+    var sum = ""
+
+    fun  show(sum:String){
+        val textView: TextView =  findViewById(R.id.textView)
+        textView.text = sum.toString()
+
+    }
+    fun press(view: View){
+        while(true){
+            sum = ClientSo(host, port).run().toString()
+            show(sum)
+        }
+    }
+}
---
+++ application/app/src/main/java/com/example/myapplication/so.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.Socket
+
+class ClientSo(host: String, port: Int) : Socket(host, port) {
+    var reader = BufferedReader(InputStreamReader(inputStream))
+    fun run(): String? {
+        while (true) {
+            val line = reader.readLine()
+            if (line != null)
+                return line
+        }
+    }
+}
