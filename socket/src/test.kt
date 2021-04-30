650c80cc2a658d47227a7278dd30ec1f42d29b2e
jabee0228 <james930228@gmail.com>
2021-04-30 09:37:00 +0800

    add test1

--- socket/src/test.kt
+++ socket/src/test.kt
@@ -1,9 +1,3 @@
 fun main(args: Array<String>) {
-    val host = "127.0.0.1"
-    val port = 2332
-    while (true) {
-        var a = ""
-        a = ClientSo(host, port).run().toString()
-        println(a)
-    }
+    println("uh")
 }
